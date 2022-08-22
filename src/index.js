d89a954b620445d29545f5184f9aea816965976f
milkshakegum <officialjmasmila@gmail.com>
2022-08-22 12:07:52 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import './styles.css'
+import App from './App'
+
+ReactDOM.render(
+  <>
+    <App />
+    <div class="overlay">
+      <a href="https://plsmilk.me">
+        <b>milk —</b>
+      </a>
+      <a class="right" href="https://codesandbox.io/s/hi-key-bubbles-iv16ml?file=/src/App.js:2343-2349">
+        /csb
+      </a>
+      <h2>
+        Light, that creative agent
+        <br />
+        the vibrations of which are the movement
+        <br />
+        and life of all things —
+      </h2>
+    </div>
+  </>,
+  document.getElementById('root')
+)
